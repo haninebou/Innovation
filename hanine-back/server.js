543de9dff49012a9bd4ebd9baf1548c87c4cc246
haninebou <bouafiahanine25@gmail.com>
2024-10-07 19:30:05 +0100

    the last update inchallah

--- hanine-back/server.js
+++ hanine-back/server.js
@@ -6,14 +6,19 @@ const productRoutes = require('./routes/productRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 const cartRoutes = require('./routes/cartRoutes');
 const orderRoutes = require('./routes/orderRoutes');
-const adminRoutes = require('./routes/adminRoutes');// Import the new order routes
+const adminRoutes = require('./routes/adminRoutes'); // Import the new order routes
 const db = require('./config/db');
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
+
+// Static files
 app.use('/uploads', express.static('uploads'));
+
+// Routes
 app.use('/auth', authRoutes);
 app.use('/categories', categoryRoutes);
 app.use(productRoutes);
@@ -22,11 +27,13 @@ app.use(cartRoutes);
 app.use(orderRoutes);  // Use the new order routes
 app.use('/admin', adminRoutes);
 
+// Root route
 app.get('/', (req, res) => {
   res.json("Welcome to the backend");
 });
 
-const PORT = 8081;
+// Port binding for deployment environments like Render
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
